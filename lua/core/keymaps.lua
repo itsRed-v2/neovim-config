05a452dfc47be4cac2563210c5b8ff144f35350e
Paul Charignon <paul.charignon.v2@gmail.com>
2025-10-19 16:56:02 +0200

    Ajout d'un raccourci pour exécuter le fichier python actuel

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -11,5 +11,20 @@ vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Focus sur la fenêtre en bas" }
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Focus sur la fenêtre en haut" });
 
 -- navigation entre les buffers
-vim.keymap.set("n", "<S-l>", "<Cmd>bnext<CR>");
-vim.keymap.set("n", "<S-h>", "<Cmd>bprevious<CR>");
+vim.keymap.set("n", "<S-l>", "<Cmd>bnext<CR>", { desc = "Focus sur le buffer à droite" });
+vim.keymap.set("n", "<S-h>", "<Cmd>bprevious<CR>", { desc = "Focus sur le buffer à gauche" });
+
+-- si dans un fichier python, executer le fichier
+vim.keymap.set("n", "<leader>R", function()
+    -- check if the current buffer is for a python file
+    if vim.bo.filetype == "python" then
+        -- get the internal representation of the "enter" key
+        local key = vim.api.nvim_replace_termcodes("<Enter>", true, false, true)
+        -- simulate a mapping being executed using nvim_feedkeys()
+        -- %:p is getting replaced by the nvim command line by the current buffer's file full path
+        vim.api.nvim_feedkeys(":!python %:p"..key, "m", false)
+    else
+        print("Buffer is not a python file")
+    end
+end, { desc = "Execute le fichier python courant" })
+
